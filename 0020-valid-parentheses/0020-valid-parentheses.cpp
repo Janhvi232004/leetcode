8227b13e471bcd5437be4afd5f1c84e56f6c8696
janhvi indapurkar <janhvi.indapurkar23@gmail.com>
2026-07-08 19:35:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if(st.empty()) return false; // stack must not be empty
+                if((st.top() == '(' && s[i] == ')') ||
+                   (st.top() == '{' && s[i] == '}') ||
+                   (st.top() == '[' && s[i] == ']')) {
+                    st.pop();
+                } else {
+                    return false; // mismatch
+                }
+            }
+        }
+        return st.empty(); // valid only if stack is empty
+    }
+};
